cc79ed853221ad86e02e3ee1bf4a51f6af7f8f5b
Eike mx <merxeike@gmail.com>
2022-04-23 12:06:24 +0100

    added news.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import news from "./news.json";
 
-function App() {
+const App = () => {
+  const displayNews = news.hits;
+  console.log(displayNews);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vader News.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
