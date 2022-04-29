767491cb77f0de86f9c16c0fabd69b42f9374d7b
predavladc <predavladc@gmail.com>
2022-04-29 22:51:53 +0200

    Added date formatting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { formatDistance, subDays, parseISO, differenceInHours, differenceInDays, differenceInWeeks, differenceInMonths } from 'date-fns';
 import './index.css';
 import news from "./news.json";
 import Footer from "./components/Footer.js"
@@ -22,7 +23,7 @@ const App = () => {
     <div className="App"   >
       <Navigation />
       {displayNews.map((element, index) => 
-       <Newsfeed news={element} key={element.objectID} index={index} /> 
+       <Newsfeed news={element} key={element.objectID} index={index} newsDate={element.created_at} /> 
       )}
       <Footer />
     </div>
--- src/components/Newsfeed.js
+++ src/components/Newsfeed.js
@@ -1,6 +1,49 @@
 import React from "react";
+import { parseISO, differenceInHours, formatDistanceToNow, differenceInDays, differenceInMonths, formatDistance   } from 'date-fns'
 
-export default function Newsfeed(props) {
+export default function Newsfeed(props, newsDate) {
+// Current date
+const dateInMs = new Date()
+// console.log(dateInMs)
+
+// const result = differenceInHours(
+//   new Date(2021, 1, 31, 11, 0),
+//   new Date(2022, 4, 29, 21, 0)
+// )
+
+// console.log(result);
+
+// News Date
+const convertedTimeFromIso = parseISO("2021-01-31T11:05:47.000Z", { additionalDigits: 1 } )
+const convervedTimeToMs = convertedTimeFromIso.getTime()
+console.log(convertedTimeFromIso);
+console.log(convervedTimeToMs);
+
+const timeDiffInDays = differenceInDays(
+  new Date(dateInMs),
+  new Date(convertedTimeFromIso)
+)
+const timeDiffInHrs = differenceInHours(
+  new Date(dateInMs),
+  new Date(convertedTimeFromIso)
+)
+const timeDiffInMonths = differenceInMonths(
+  new Date(dateInMs),
+  new Date(convertedTimeFromIso)
+)
+
+const diff = formatDistance(
+  new Date(convertedTimeFromIso),
+  new Date(dateInMs),
+  { addSuffix: true }
+)
+console.log(diff);
+// console.log(timeDiffInDays, timeDiffInHrs, timeDiffInMonths ); 
+
+// if (timeDiffInMonths < 1) {
+//   return <h1> posted  {differenceInDays} ago</h1>
+// } else if { }
+
   return (
     <div>
         <p>{props.index+1}</p>
@@ -9,6 +52,7 @@ export default function Newsfeed(props) {
         <p>{props.news.points}</p>
         <p>by {props.news.author}</p>
         <p>{props.news.created_at} | hide |</p>
+        <p> posted {diff} | hide |</p>
         <p>{props.news.num_comments}</p>
     </div>
   );
