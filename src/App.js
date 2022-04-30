eeb1b4a0e4ee038f3999ba49c25ee0c64efdac97
Eike mx <merxeike@gmail.com>
2022-04-30 15:04:57 +0100

    changed from news.json to live API

--- src/App.js
+++ src/App.js
@@ -3,21 +3,49 @@ import React, {useState, useEffect} from 'react';
 
 import './index.css';
 
-import news from "./news.json";
+// import news from "./news.json";
 import Footer from "./components/Footer.js"
 import Navigation from "./components/Navigation"
 import Newsfeed from './components/Newsfeed';
 
 const App = () => {
 
-  const displayNews = news.hits;
- // console.log(displayNews);
+  const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("http://hn.algolia.com/api/v1/search_by_date?query=react&tags=story")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Random 404");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setNews(data.hits);
+        setIsLoading(false)
+      })
+      .catch((e) => {
+        setIsLoading(false)
+        setIsError(true)
+      })
+  }, []);
+
+  if (isError) {
+    return <h5>No News are good News!</h5>
+  }
+  // console.log(news);
+
+  const displayNews = news;
+
+//  console.log(displayNews);
 
   return (
     <div className="App news-wrapper"   >
       <Navigation />
       <div className='newsfeed-wrapper'>
-        {}
         {displayNews.filter((element) => element.url).map((element, index) => 
         <Newsfeed news={element} key={element.objectID} index={index} /> 
         )}
